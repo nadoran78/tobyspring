02684bb8ce93c63aaa450b30f6088e9ed0ba65d3
nadoran78 <nadoran78@gmail.com>
2024-03-25 11:53:28 +0900

    init

--- src/main/java/springbook/user/Main.java
+++ src/main/java/springbook/user/Main.java
@@ -1,13 +1,11 @@
 package springbook.user;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TobyspringApplication {
+public class Main {
 
   public static void main(String[] args) {
-    SpringApplication.run(TobyspringApplication.class, args);
+    System.out.println("init");
   }
 
 }
