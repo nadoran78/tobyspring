aa29608d1d6b6afca406ac322005a4ce95e75de2
nadoran78 <nadoran78@gmail.com>
2024-03-28 11:40:48 +0900

    1.8 XML을 이용한 설정

--- src/main/java/springbook/user/UserDaoTest.java
+++ src/main/java/springbook/user/UserDaoTest.java
@@ -1,22 +1,23 @@
 package springbook.user;
 
 import java.sql.SQLException;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import springbook.user.dao.DaoFactory;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
+@SpringBootApplication
 public class UserDaoTest {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("whiteship3");
+        user.setId("whiteship5");
         user.setName("홍길동");
         user.setPassword("married");
 
--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,15 +1,31 @@
 package springbook.user.dao;
 
+import java.sql.Driver;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 public class DaoFactory {
 
   @Bean
-  public UserDao userDao() {
+  public DataSource dataSource() throws ClassNotFoundException {
+    SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+    dataSource.setDriverClass(
+        (Class<? extends Driver>) Class.forName("com.mysql.cj.jdbc.Driver"));
+    dataSource.setUrl("jdbc:mysql://localhost/springbook");
+    dataSource.setUsername("root");
+    dataSource.setPassword("toby");
+
+    return dataSource;
+  }
+
+  @Bean
+  public UserDao userDao() throws ClassNotFoundException {
     UserDao userDao = new UserDao();
-    userDao.setConnectionMaker(connectionMaker());
+    userDao.setDataSource(dataSource());
     return userDao;
   }
 
