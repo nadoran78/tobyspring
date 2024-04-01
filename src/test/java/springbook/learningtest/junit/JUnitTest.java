51e85b78b6c713a33c1ff5d50c57c068c652329c
nadoran78 <nadoran78@gmail.com>
2024-04-01 14:19:46 +0900

    2.5 학습 테스트로 배우는 스프링

---
+++ src/test/java/springbook/learningtest/junit/JUnitTest.java
@@ -0,0 +1,56 @@
+package springbook.learningtest.junit;
+
+import static org.hamcrest.CoreMatchers.either;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
+public class JUnitTest {
+  @Autowired
+  ApplicationContext context;
+  static Set<JUnitTest> testObjects = new HashSet<>();
+  static ApplicationContext contextObject = null;
+
+  @Test
+  public void test1() {
+    assertNotEquals(testObjects, hasItem(this));
+    testObjects.add(this);
+
+    assertTrue(contextObject == null || contextObject == this.context);
+    contextObject = this.context;
+  }
+
+  @Test
+  public void test2() {
+    assertNotEquals(testObjects, hasItem(this));
+    testObjects.add(this);
+
+    assertTrue(contextObject == null || contextObject == this.context);
+    contextObject = this.context;
+  }
+
+  @Test
+  public void test3() {
+    assertNotEquals(testObjects, hasItem(this));
+    testObjects.add(this);
+
+    assertTrue(contextObject == null || contextObject == this.context);
+    contextObject = this.context;
+  }
+}
