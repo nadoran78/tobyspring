0dad557f21c1a5a6b3e51ce92502de4186ed5065
nadoran78 <nadoran78@gmail.com>
2024-04-03 11:04:52 +0900

    3.2 변하는 것과 변하지 않는 것

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -52,37 +52,16 @@ public class UserDao {
     ps.close();
     c.close();
 
-    if (user == null) throw new EmptyResultDataAccessException(1);
+    if (user == null) {
+      throw new EmptyResultDataAccessException(1);
+    }
 
     return user;
   }
 
   public void deleteAll() throws SQLException {
-    Connection c = null;
-    PreparedStatement ps = null;
-
-    try {
-      c = dataSource.getConnection();
-      ps = c.prepareStatement("delete from users");
-      ps.executeUpdate();
-    } catch (SQLException e) {
-      throw e;
-    } finally {
-      if (ps != null) {
-        try {
-          ps.close();;
-        } catch (SQLException e) {
-
-        }
-      }
-      if (c != null) {
-        try {
-          c.close();
-        } catch (SQLException e) {
-
-        }
-      }
-    }
+    StatementStrategy st = new DeleteAllStatement();
+    jdbcContextWithStatementStrategy(st);
   }
 
   public int getCount() throws SQLException {
@@ -110,7 +89,39 @@ public class UserDao {
       }
       if (ps != null) {
         try {
-          ps.close();;
+          ps.close();
+          ;
+        } catch (SQLException e) {
+
+        }
+      }
+      if (c != null) {
+        try {
+          c.close();
+        } catch (SQLException e) {
+
+        }
+      }
+    }
+  }
+
+  public void jdbcContextWithStatementStrategy(StatementStrategy stmt)
+      throws SQLException {
+    Connection c = null;
+    PreparedStatement ps = null;
+
+    try {
+      c = dataSource.getConnection();
+
+      ps = stmt.makePreparedStatement(c);
+
+      ps.executeUpdate();
+    } catch (SQLException e) {
+      throw e;
+    } finally {
+      if (ps != null) {
+        try {
+          ps.close();
         } catch (SQLException e) {
 
         }
